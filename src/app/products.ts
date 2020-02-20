768442e9c4d564a652917f38b5e340cd069fdd00
sandibaeva52 <sandibaeva52@gmail.com>
2020-02-20 13:14:44 +0600

    week

---
+++ src/app/products.ts
@@ -0,0 +1,79 @@
+export const products = [
+    {
+      name: 'Koala Bear',
+      price: 799,
+      image:"https://ae01.alicdn.com/kf/H8e18082f9ac94b358d7969b1dbaf2dadR/16CM-New-Arrival-Super-Cute-Small-Koala-Bear-Plush-Toys-Adventure-Koala-Doll-Birthday-Christmas-Gift.jpg",
+      description: ' New Arrival Super Cute Small Koala Bear Plush Toys Adventure Koala Doll Birthday Christmas Gift PT024',
+      link: "https://www.aliexpress.com/item/4000691656468.html?spm=2114.12010615.8148356.3.76bd172aMm4LDb"
+    },
+    {
+     name: 'Big Shark doll',
+      price: 120,
+      image:"https://ae01.alicdn.com/kf/Hadcc45db89164b36869edb39eda44b8eK/55-115cm-Big-shark-doll-plush-toy-sleeping-pillow-on-the-bed-doll-long-pillow.jpg",
+      description: ' Big shark doll plush toy sleeping pillow on the bed doll long pillow',
+      link: "https://www.aliexpress.com/item/4000621057229.html?spm=a2g0o.home.15002.1.650c2c25gI3880&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=dd6756c3-f5f2-4fc9-a121-9efacc8cd3b6&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:dd6756c3-f5f2-4fc9-a121-9efacc8cd3b6,tpp_buckets:668%230%23131923%2318_668%23808%235881%23663_668%23888%233325%235"
+    },
+   {
+     name: 'Sports Bottle',
+      price: 399,
+      image:"https://ae01.alicdn.com/kf/Hd840bd73cacc4f66809506eef8e97fcf0/Stainless-Steel-Sports-Water-Bottle-Insulated-Vacuum-Flask-Single-Wall-Hot-Cold-Water-Bottle-350-500.jpg",
+      description: 'Stainless Steel Sports Water Bottle Insulated Vacuum Flask Single Wall Hot Cold Water Bottle 350/500/750/1000ml bouteille inox',
+      link: "https://www.aliexpress.com/item/4000291507432.html?spm=a2g0o.home.15002.3.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=fa32c08e-ca1a-40aa-a4cf-f68930c240fb&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:fa32c08e-ca1a-40aa-a4cf-f68930c240fb,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Watches',
+      price: 599,
+      image:"https://ae01.alicdn.com/kf/Ha230d3bdc687456eb1e51e0fe4137657l/-.jpg",
+      description: ' Роскошные мужские и женские спортивные повседневные светодиодный часы, цифровые часы с подсветкой, водонепроницаемые наручные часы, montre femme Relogio Masculino',
+      link: "https://aliexpress.ru/item/4000558020769.html?spm=a2g0o.home.15002.23.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=fa32c08e-ca1a-40aa-a4cf-f68930c240fb&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:fa32c08e-ca1a-40aa-a4cf-f68930c240fb,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Ring',
+      price: 699,
+      image:"https://ae01.alicdn.com/kf/HTB1aLrPlcj_B1NjSZFHq6yDWpXao/15.jpg",
+      description: ' 15 шт./компл., богемное Ретро хрустальное кольцо с цветком и листьями лотоса, серебряное кольцо, Женский Свадебный Подарок на годовщину',
+      link: "https://aliexpress.ru/item/32897181226.html?spm=a2g0o.home.15002.42.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=4b5c7774-f852-46ef-b135-4f1d9b399fc7&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:4b5c7774-f852-46ef-b135-4f1d9b399fc7,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Bracelet',
+      price: 99,
+      image:"https://ae01.alicdn.com/kf/H13969361e81c48ea9ea225eb6ea25e39o/Mostyle-5.jpg",
+      description: 'Mostyle, 5 шт./набор, богемный, золотистого цвета луна, лист, кристалл, опал, Открытый браслет, набор для женщин, в стиле панк, бохо, пляжный браслет, ювелирное изделие, подарок',
+      link: "https://aliexpress.ru/item/4000047083378.html?spm=a2g0o.home.15002.50.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=ad8ec73d-fbc1-49f8-8ea4-26446c38506f&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:ad8ec73d-fbc1-49f8-8ea4-26446c38506f,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Accessories',
+      price: 199,
+      image:"https://ae01.alicdn.com/kf/HTB1MwjPV4jaK1RjSZKzq6xVwXXak/1-Baitie.jpg",
+      description: ' 1 шт. простой элегантный стиль Baitie жемчужная Дамская Золотая заколка для волос заколка для невесты заколка для прически аксессуары для укладки4',
+      link: "https://aliexpress.ru/item/33016392363.html?spm=a2g0o.home.15002.76.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=ddead394-8ed0-4766-88d9-6ef5ffe74a17&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:ddead394-8ed0-4766-88d9-6ef5ffe74a17,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Eye patches',
+      price: 799,
+      image:"https://ae01.alicdn.com/kf/H8a83884cb1564ff2a1eaae0de3d5bd4bf/60-24K.jpg",
+      description: ' 60 шт. 24K Золотая коллагеновая маска для глаз против морщин и сна с кристаллами увлажняющее устройство для удаления темных кругов маска для глаз Уход за глазами',
+      link: "https://aliexpress.ru/item/4000074470823.html?spm=a2g0o.home.15002.87.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=ddead394-8ed0-4766-88d9-6ef5ffe74a17&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:ddead394-8ed0-4766-88d9-6ef5ffe74a17,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Luxury Bracelet',
+      price: 799,
+      image:"https://ae01.alicdn.com/kf/H4bee009616b5484da9f8f745bbc09788K/Fashion-5pcs-Set-Women-Watches-Luxury-Magnet-Buckle-Flower-Rhinestone-Watch-Ladies-Quartz-Wrist-Watch-Bracelet.jpg",
+      description: ' Fashion 5pcs Set Women Watches Luxury Magnet Buckle Flower Rhinestone Watch Ladies Quartz Wrist Watch Bracelet Set Reloj Mujer',
+      link: "https://www.aliexpress.com/item/4000598819161.html?spm=a2g0o.home.15002.26.76712c25Nlm4Er&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=4b5c7774-f852-46ef-b135-4f1d9b399fc7&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:4b5c7774-f852-46ef-b135-4f1d9b399fc7,tpp_buckets:668%230%23131923%2318_668%23808%234094%23487_668%23888%233325%235"
+    },
+    {
+     name: 'Koala Bear',
+      price: 799,
+      image:"https://ae01.alicdn.com/kf/H35de2baaea974218a1dab5a7331bed0bY/-.jpg",
+      description: 'Ветронепроницаемый трехслойный автоматический зонт от дождя для женщин авто Роскошные Большие ветрозащитные Зонты Мужская рамка ветрозащитный 10K зонтик',
+      link: "https://aliexpress.ru/item/32867103487.html?spm=a2g0o.tm76917.2762158730.12.410d2df072qVuL&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=e65c49ed-035d-4887-a167-9a5cdd758aea"
+    }
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at http://angular.io/license
+  */
